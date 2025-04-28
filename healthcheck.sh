6d9a0dcdd4fc7ac79450c4e4f165adf41628aa04
Oleksander Piskun <oleksandr2088@icloud.com>
2025-04-28 13:42:22 +0300

    feat: added HaRP support (Nextcloud 32+)
    Signed-off-by: Oleksander Piskun <oleksandr2088@icloud.com>

---
+++ healthcheck.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+# SPDX-License-Identifier: AGPL-3.0-or-later
+if [ -f /frpc.toml ] && [ -n "$HP_SHARED_KEY" ]; then
+  if pgrep -x "frpc" > /dev/null; then
+      exit 0
+  else
+      exit 1
+  fi
+fi
